67bb293b09211d4779c76cd4133880e89aeb8ff1
Abir Hasan <hackitabirhasan@gmail.com>
2020-12-24 14:25:58 +0600

    Testing: 01

--- bot/plugins/call_back_button_handler.py
+++ bot/plugins/call_back_button_handler.py
@@ -54,6 +54,7 @@ async def button(bot, update: CallbackQuery):
                         pass
                     try:
                         await update.message.edit_text("🚦🚦 Last Process Stopped 🚦🚦")
+			await bot.send_message(update.message.chat.id, "Process Cancelled!")
                     except:
                         pass
             else:
