b6a7cecfc8ca6a6b0ff18a305b7568bf906a0716
Abir Hasan <hackitabirhasan@gmail.com>
2021-02-01 03:19:20 +0600

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -6,7 +6,7 @@ from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Compress HEVC Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file without losing video quality as HEVC Video!</b> \n\n/help for more details. \n\nSupport Group: @Linux_Repo"
+    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Compress Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details. \n\nSupport Group: @Linux_Repo"
    
     ABS_TEXT = " Please don't be selfish."
     
@@ -47,7 +47,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi, I am Video Compressor HEVC Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: /compress \n\nSupport Group: @Linux_Repo"
+        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @Linux_Repo"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
