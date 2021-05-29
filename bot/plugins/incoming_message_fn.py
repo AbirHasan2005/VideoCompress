ef4fea175b55980e0a214f5d42bc283b8c56e64a
Mugeñ ®ao <84485574+theRawrrr@users.noreply.github.com>
2021-05-29 20:02:13 +0530

    Update incoming_message_fn.py
    While we deploying without any changes in it, The `bot status` does not get to us into our channel.. It will confuse some users.. So leave it as simple like i propose or edit with your channel user name sir.. Thank You

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -260,7 +260,7 @@ async def incoming_compress_message_f(bot, update):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton('Show Bot Status', url=f'https://t.me/{LOG_CHANNEL}')
+                            InlineKeyboardButton('Show Bot Status', url='https://t.me/yourchannel_username')
                             # That's Username na ...
                         ]
                     ]
